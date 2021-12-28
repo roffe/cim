a3b93b55a49bea37afb9e3c5ed6aa1a2325af903
Roffe <roffe@roffe.nu>
2021-12-28 18:37:31 +0100

    add sync validation

--- pkg/cim/sync.go
+++ pkg/cim/sync.go
@@ -1,5 +1,11 @@
 package cim
 
+import (
+	"fmt"
+
+	"github.com/roffe/cim/pkg/crc16"
+)
+
 type Sync struct {
 	Data     [][]byte `bin:"len:5,[len:4]"`
 	Checksum uint16   `bin:"le,len:2"`
@@ -9,4 +15,19 @@ func (s *Sync) SetData(no uint8, data []byte) {
 	s.Data[no] = data
 }
 
-//func (s *Sync)
+func (s *Sync) validate() error {
+	if s.Checksum != s.Crc16() {
+		if s.Checksum != s.Crc16() {
+			return fmt.Errorf("sync data checksum does not match calculated checksum")
+		}
+	}
+	return nil
+}
+
+func (p *Sync) Crc16() uint16 {
+	var data []byte
+	for _, b := range p.Data {
+		data = append(data, b...)
+	}
+	return crc16.Calc(data)
+}
